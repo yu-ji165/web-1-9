7f0103ab53b9259c7fc1a61e8355b9f752590542
yu-ji <enpatsushakugan@icloud.com>
2018-07-09 18:11:01 +0900

    疲れた

---
+++ AppleCatch/Assets/BasC.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasC : MonoBehaviour
+{
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource and;
+    GameObject director;
+    // Use this for initialization
+    void Start()
+    {
+        this.director = GameObject.Find("GameD");
+        this.and = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            this.director.GetComponent<GameD>().GetApple();
+            this.and.PlayOneShot(this.appleSE);
+        }
+        else
+        {
+            this.director.GetComponent<GameD>().GetBomb();
+            this.and.PlayOneShot(this.bombSE);
+        }
+        //Debug.Log("キャッチ！");
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+
+    }
+}
---
+++ AppleCatch/Assets/itemG.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemG : MonoBehaviour
+{
+
+    public GameObject appleP;
+    public GameObject bombP;
+    float spin = 1.0f;
+    float delta = 0;
+    int ratio = 2;
+    float speed = -0.03f;
+
+    public void SetP(float spin,float speed,int ratio)
+    {
+        this.spin = spin;
+        this.speed = speed;
+        this.ratio = ratio;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.spin)
+        {
+            this.delta = 0;
+            GameObject item;
+            int dice = Random.Range(1, 11);
+            if (dice <= this.ratio)
+            {
+                item = Instantiate(bombP) as GameObject;
+            }
+            else
+            {
+                item = Instantiate(appleP) as GameObject;
+            }
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            item.transform.position = new Vector3(x, 4, z);
+            item.GetComponent<itemC>().dropSpeed = this.speed;
+        }
+    }
+}
